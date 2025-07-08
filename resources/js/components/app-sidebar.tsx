3fb59d8954a549e4c593a229af4c24cbc339cdc5
Raphael Ramos <raphael.opensource@gmail.com>
2025-07-08 19:35:09 -0300

    feat(app-sidebar.tsx): uncomment NavUser

--- resources/js/components/app-sidebar.tsx
+++ resources/js/components/app-sidebar.tsx
@@ -1,10 +1,10 @@
 import { NavFooter } from '@/components/nav-footer';
 import { NavMain } from '@/components/nav-main';
-// import { NavUser } from '@/components/nav-user';
+import { NavUser } from '@/components/nav-user';
 import { Sidebar, SidebarContent, SidebarFooter, SidebarHeader, SidebarMenu, SidebarMenuButton, SidebarMenuItem } from '@/components/ui/sidebar';
 import { type NavItem } from '@/types';
 import { Link } from '@inertiajs/react';
-import { BookOpen, Folder, LayoutGrid } from 'lucide-react';
+import { BookOpen, Folder, LayoutGrid, LogOutIcon } from 'lucide-react';
 import AppLogo from './app-logo';
 
 const mainNavItems: NavItem[] = [
@@ -47,10 +47,10 @@ export function AppSidebar() {
                 <NavMain items={mainNavItems} />
             </SidebarContent>
 
-            {/*<SidebarFooter>*/}
-            {/*    <NavFooter items={footerNavItems} className="mt-auto" />*/}
-                {/*<NavUser />*/}
-            {/*</SidebarFooter>*/}
+            <SidebarFooter>
+               {/* <NavFooter items={footerNavItems} className="mt-auto" /> */}
+                <NavUser />
+            </SidebarFooter>
         </Sidebar>
     );
 }
