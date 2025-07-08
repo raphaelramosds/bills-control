1f8e8b38dd62c5a3b4e62920baf58ab59f5e1c01
Raphael Ramos <raphael.opensource@gmail.com>
2025-07-08 19:35:56 -0300

    fix: interface redesign

--- resources/js/pages/bills/index.tsx
+++ resources/js/pages/bills/index.tsx
@@ -38,7 +38,7 @@ export default function Index({ ...props }) {
     const handleClear = () => {
         get(route('bills.index'), {
             preserveScroll: true,
-            preserveState: true
+            preserveState: false
         });
     };
 
@@ -48,50 +48,37 @@ export default function Index({ ...props }) {
             <main className="flex h-full flex-1 flex-col gap-4 rounded-xl p-10">
                 <header className="flex">
                     <h1 className="font-bold text-lg flex-1">Listar contas</h1>
-                    <button
-                        onClick={() => {
-                            setSelectedBill({});
-                            setOpenFormDialog(true);
-                        }}
-                        className="cursor-pointer w-[180px] rounded-sm border text-center px-5 py-1.5 text-sm leading-normal text-[#1b1b18] hover:border-[#19140035] dark:text-[#EDEDEC] dark:hover:border-[#3E3E3A]"
-                    >
-                        Cadastrar
-                    </button>
                 </header>
-                <section className="flex gap-4">
-                    <div className="flex flex-col gap-2 flex-1">
+                <section className="flex gap-4 ">
+                    <div className="flex flex-col gap-2 flex-1 border rounded-xl">
                         {props.bills.length > 0 ?
                             props.bills.map(bill => (
-                                <div className="flex p-4 gap-5 rounded border" key={bill.id}>
+                                <div className="flex p-3 gap-7 rounded" key={bill.id}>
+
                                     <div className="text-left">
-                                        <div
-                                            className={`font-bold ${bill.payment_date ? 'text-sky-500' : 'text-red-500'}`}>
-                                            <span>{asCurrency(bill.amount)}</span>
-                                        </div>
-                                        <div className="flex items-center">
-                                            <span className="relative flex size-3">
+                                        <div>
+                                            <span className="relative">
                                                 <span
                                                     className={`absolute inline-flex h-full w-full animate-ping rounded-full ${bill.payment_date ? 'bg-sky-400' : 'bg-red-400'} opacity-75`}></span>
                                                 <span
-                                                    className={`relative inline-flex size-3 rounded-full ${bill.payment_date ? 'bg-sky-500' : 'bg-red-500'}`}></span>
+                                                    className={`relative inline-flex size-3 rounded-full ${bill.payment_date ? 'bg-chart-2' : 'bg-red-500'}`}></span>
                                             </span>
-                                            <span className="ml-1">
-                                                {bill.payment_date ? `Pago` : 'Pendente'}
-                                            </span>
+                                            <span className='ml-2'>{asCurrency(bill.amount)}</span>
                                         </div>
                                     </div>
 
                                     <div className="flex-1">
                                         <h1>{bill.name}</h1>
-                                        <span className="text-sm italic">
-                                            {bill.payment_date ? `Pago em ${asDate(bill.payment_date)}` : `Vence em ${asDate(bill.expiration_date)}`}
-                                        </span>
                                     </div>
 
                                     <div className="flex items-center">
                                         <DropdownMenu>
                                             <DropdownMenuTrigger asChild>
-                                                <EllipsisVertical className="ml-auto size-4 cursor-pointer" />
+                                                <button className={`cursor-pointer rounded text-sm text-white/80 ${bill.payment_date ? 'bg-chart-2/80' : 'bg-red-500/80'} p-1`}>
+                                                    <span>
+                                                        {bill.payment_date ? `Pago em ${asDate(bill.payment_date)}` : `Vence em ${asDate(bill.expiration_date)}`}
+                                                    </span>
+                                                </button>
                                             </DropdownMenuTrigger>
                                             <DropdownMenuContent
                                                 className="w-(--radix-dropdown-menu-trigger-width) min-w-56 rounded-lg"
@@ -120,7 +107,7 @@ export default function Index({ ...props }) {
                                                 <DialogHeader>
                                                     <DialogTitle>{Object.keys(selectedBill).length === 0 ? 'Cadastrar conta' : 'Modificar conta'}</DialogTitle>
                                                     <DialogDescription>
-                                                        <Form bill={selectedBill}/>
+                                                        <Form bill={selectedBill} />
                                                     </DialogDescription>
                                                 </DialogHeader>
                                             </DialogContent>
@@ -130,21 +117,44 @@ export default function Index({ ...props }) {
                             ))
                             : 'Nenhuma conta foi cadastrada'}
                     </div>
-                    <div className="flex flex-col gap-3 w-1/4 p-3 border rounded max-h-fit">
-                        <Select onValueChange={handleMonthSearch}>
-                            <SelectTrigger>
-                                <SelectValue placeholder="Vencimento" />
-                            </SelectTrigger>
-                            <SelectContent>
-                                {props.months.map((month) => <SelectItem key={month}
-                                    value={month}>{asMonthYear(month)}</SelectItem>)}
-                            </SelectContent>
-                        </Select>
-                        <Button className="w-full cursor-pointer mt-3" type="button" variant="outline"
-                            onClick={handleClear}>
-                            Redefinir
-                        </Button>
-                    </div>
+                    <aside className="flex flex-col gap-3 w-1/4 max-h-fit">
+                        <div className='flex flex-col gap-3 rounded border p-3'>
+                            <div>
+                                <span className='text-sm'>Filtros</span>
+                            </div>
+                            <Select onValueChange={handleMonthSearch}>
+                                <SelectTrigger>
+                                    <SelectValue placeholder="Vencimento" />
+                                </SelectTrigger>
+                                <SelectContent>
+                                    {props.months.map((month) => <SelectItem key={month}
+                                        value={month}>{asMonthYear(month)}</SelectItem>)}
+                                </SelectContent>
+                            </Select>
+                            <button className="cursor-pointer w-full rounded-sm border text-center px-5 py-1.5 text-sm leading-normal text-[#1b1b18] hover:border-[#19140035] dark:text-white dark:hover:border-[#3E3E3A] dark:bg-input" type="button"
+                                onClick={handleClear}>
+                                Redefinir
+                            </button>
+                            <button
+                                onClick={() => {
+                                    setSelectedBill({});
+                                    setOpenFormDialog(true);
+                                }}
+                                className="cursor-pointer w-full rounded-sm border text-center px-5 py-1.5 text-sm leading-normal text-[#1b1b18] hover:border-[#19140035] dark:text-white dark:hover:border-[#3E3E3A] dark:bg-chart-2"
+                            >
+                                Nova conta
+                            </button>
+                        </div>
+                        <div className='flex gap-3 flex-col rounded border p-3'>
+                            <div>
+                                <span className='text-sm'>Totais</span>
+                            </div>
+                            <div className='w-full flex'>
+                                <span className='rounded-l-lg flex-1 bg-chart-2 text-sm text-center text-white p-1'>R$ 12,90</span>
+                                <span className='rounded-r-lg bg-red-500 text-sm text-center text-white p-1'>R$ 1902,90</span>
+                            </div>
+                        </div>
+                    </aside>
                 </section>
             </main>
         </AppLayout>
